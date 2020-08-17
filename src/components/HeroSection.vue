422769097328257f5d14f6bdd0f03ad86bf1babf
Florian Schildwaechter <florian.schildwaechter@bauerxcel.de>
2020-08-17 14:44:30 +0200

    feat: added footer and header // added hero section

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="h-75 bg-red-500 flex flex-wrap justify-center items-center relative">
+    <h2 class="text-white flex-col text-6xl w-9/12 text-center">Strom speichern – ökologisch UND ökonomisch!</h2>
+    <div class="bg-blue-400 flex-col w-4 h-4 absolute bottom-0"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HeroSection'
+}
+</script>
+
+<style lang="scss" scoped>
+.h-75 {
+  height: 75vh;
+}
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,10 @@
 <template>
   <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot/>
+    <Header />
+    <div class="min-h-screen">
+      <slot />
+    </div>
+    <Footer />
   </div>
 </template>
 
@@ -21,30 +16,16 @@ query {
 }
 </static-query>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
+export default {
+  components: {
+    Header,
+    Footer
+  }
 }
+</script>
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
+<style></style>
