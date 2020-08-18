94e6f41b7b7af6bc776c8895912370c5702d6c72
Florian Schildwaechter <florian.schildwaechter@bauerxcel.de>
2020-08-18 13:49:20 +0200

    feat: added footer

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -5,43 +5,43 @@
         <div class="flex flex-wrap -m-2">
           <div class="p-2 w-1/2">
             <input
-              class="w-full bg-transparent rounded border-2 border-black focus:outline-none focus:border-blue-800 text-base px-4 py-2"
+              class="w-full bg-transparent border-2 border-black focus:outline-none focus:border-blue-800 text-base px-4 py-2"
               placeholder="Vorname"
               type="text"
             />
           </div>
           <div class="p-2 w-1/2">
             <input
-              class="w-full bg-transparent rounded border-2 border-black focus:outline-none focus:border-blue-800 text-base px-4 py-2"
+              class="w-full bg-transparent border-2 border-black focus:outline-none focus:border-blue-800 text-base px-4 py-2"
               placeholder="Nachname"
               type="text"
             />
           </div>
           <div class="p-2 w-1/2">
             <input
-              class="w-full bg-transparent rounded border-2 border-black focus:outline-none focus:border-blue-800 text-base px-4 py-2"
+              class="w-full bg-transparent border-2 border-black focus:outline-none focus:border-blue-800 text-base px-4 py-2"
               placeholder="Betreff"
               type="text"
             />
           </div>
           <div class="p-2 w-1/2">
             <input
-              class="w-full bg-transparent rounded border-2 border-black focus:outline-none focus:border-blue-800 text-base px-4 py-2"
+              class="w-full bg-transparent border-2 border-black focus:outline-none focus:border-blue-800 text-base px-4 py-2"
               placeholder="Email"
               type="email"
             />
           </div>
           <div class="p-2 w-full">
             <textarea
-              class="w-full bg-transparent rounded border-2 border-black focus:outline-none h-48 focus:border-blue-800 text-base px-4 py-2 resize-none block"
+              class="w-full bg-transparent border-2 border-black focus:outline-none h-48 focus:border-blue-800 text-base px-4 py-2 resize-none block"
               placeholder="Message"
             ></textarea>
           </div>
           <div class="p-2 w-full">
             <button
-              class="flex mx-auto text-white bg-blue-800 border-0 py-2 px-8 focus:outline-none hover:bg-blue-800 rounded text-lg"
+              class="flex ml-auto text-white bg-blue-800 border-0 py-2 px-8 focus:outline-none text-lg hover:shadow-xl"
             >
-              Button
+              Senden
             </button>
           </div>
         </div>
@@ -57,7 +57,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-::placeholder{
-    color: black;
+::placeholder {
+  color: black;
 }
 </style>
