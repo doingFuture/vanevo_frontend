a2f3a3484ea12de9beaacfd56b404c72ad4a5bce
Florian Schildwaechter <florian.schildwaechter@bauerxcel.de>
2020-08-30 21:34:59 +0200

    added content for datenschutz

---
+++ src/components/TextParagraph.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full py-5">
+    <h3 class="text-lg font-semibold py-2">{{ paragraph.headline }}</h3>
+    <p class="text-base">{{ paragraph.text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextParagraph',
+  props: {
+    paragraph: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/Datenschutz.vue
+++ src/pages/Datenschutz.vue
@@ -1,11 +1,36 @@
 <template>
-  <Layout></Layout>
+  <Layout>
+    <base-section background="white">
+      <base-row>
+        <text-paragraph
+          v-for="(paragraph, index) in data.paragraphs"
+          :key="index"
+          :paragraph="data.paragraphs[index]"
+        />
+      </base-row>
+    </base-section>
+  </Layout>
 </template>
 
 <script>
+import BaseSection from '@/components/BaseSection.vue'
+import BaseRow from '@/components/BaseRow.vue'
+import TextParagraph from '@/components/TextParagraph.vue'
+import Content from '@/data/datenschutz.json'
+
 export default {
   metaInfo: {
     title: 'Datenschutz'
+  },
+  components: {
+    BaseSection,
+    BaseRow,
+    TextParagraph
+  },
+  data() {
+    return {
+      data: Content
+    }
   }
 }
 </script>
