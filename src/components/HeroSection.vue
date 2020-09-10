f4ee13681ce877e505e6d7ea6c627cfb5c0d89d5
Sven Zühlsdorf <sven.zuehlsdorf@argonauten.de>
2020-09-10 21:56:51 +0200

    add new shizzle

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -9,32 +9,20 @@
       <h2
         class="text-black flex-col text-xl md:text-5xl xl:text-6xl font-bold tracking-wide"
       >
-        {{ text }}
+        STROM.
+        <br>
+        SAUBER.
+        <br>
+        SPEICHERN.
       </h2>
-      <g-link to="/#contact"
-        class="inline-block uppercase text-white self-start bg-vanevo-blue border-0 mt-8 py-4 px-8 focus:outline-none text-xs md:text-base xl:text-lg shadow-xl hover:shadow-2xl"
-      >
-        Jetzt Kontakt aufnehmen
-      </g-link>
     </div>
   </section>
 </template>
 
-<script>
-export default {
-  name: 'HeroSection',
-  props: {
-    text: {
-      type: String,
-      default: ''
-    }
-  }
-}
-</script>
+
 
 <style lang="scss" scoped>
 .h-75 {
   height: 75vh;
-
 }
 </style>
