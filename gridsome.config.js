45cc471ae13333b4f58c34666d7c721cd3071c07
Flo <florian.schildwaechter@gmail.com>
2021-02-06 16:57:47 +0100

    feat: initial netlify cms

--- gridsome.config.js
+++ gridsome.config.js
@@ -13,5 +13,28 @@ module.exports = {
         plugins: [tailwindcss]
       }
     }
-  }
+  },
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link'
+    }
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post'
+      }
+    },
+    {
+      use: 'gridsome-plugin-netlify-cms',
+      options: {
+        publicPath: '/admin'
+      }
+    }
+  ]
 }
