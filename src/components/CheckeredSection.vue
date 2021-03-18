7828e9ac6ef7ef30e8d10e36f3971ee973b4fac6
Flo <florian.schildwaechter@gmail.com>
2021-03-18 14:17:30 +0100

    feature: image compression

--- src/components/CheckeredSection.vue
+++ src/components/CheckeredSection.vue
@@ -3,11 +3,20 @@
     class="flex flex-col mb-10 lg:mb-0"
     :class="isEven ? 'lg:flex-row' : 'lg:flex-row-reverse'"
   >
-    <g-image class="lg:w-1/2 w-full h-full" :src="`/images/${image}`" />
+    <g-image
+      v-if="isEven"
+      class="lg:w-1/2 w-full h-full"
+      src="~/assets/images/was_01.jpg"
+    />
+    <g-image
+      v-else
+      class="lg:w-1/2 w-full h-full"
+      src="~/assets/images/was_02.jpg"
+    />
     <div
       class="lg:w-1/2 w-full bg-white flex flex-col justify-center items-center px-0 lg:px-5 py-10 lg:p-0"
     >
-      <p class="w-10/12  ml-5">
+      <p class="w-10/12 ml-5">
         {{ content }}
       </p>
       <slot />
@@ -18,6 +27,7 @@
 <script>
 export default {
   name: 'CheckeredSection',
+
   props: {
     reverse: {
       type: Boolean,
